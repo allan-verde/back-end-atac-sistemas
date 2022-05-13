46a2274c48f716c2ae8d5456e765d464a6880352
allan <allangreen2906@gmail.com>
2022-05-13 19:26:17 -0300

    feat: Added new environment variable

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,18 @@
+import dotenv from 'dotenv'
 import 'reflect-metadata'
 import app from './app'
 import { AppDataSource } from './db'
 
 
+dotenv.config()
+
+const port = process.env.PORT || 3333
+
 async function main() {
     try {
         await AppDataSource.initialize()
         console.log('Database conected')
-        app.listen(3000, () => console.log('Server is runnig'))
+        app.listen(port, () => console.log('Server is runnig'))
     } catch (error) {
         console.log(error)
     }
